a2d8ec7fa5ff8469702c179ca232380ed3106e7e
Wenxin <wt753@nyu.edu>
2021-06-25 14:59:52 +0800

    init

---
+++ Chile/ChileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChileApp.swift
+//  Chile
+//
+//  Created by 唐文心 on 2021/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ChileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chile
+//
+//  Created by 唐文心 on 2021/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
